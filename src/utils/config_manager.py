c920a5d65ea60e2986a0975c4d8b8198aae348ca
MiniX16 <javivi004.ou@gmail.com>
2025-04-05 12:05:15 +0200

    configManager takes fixes path with username

--- src/utils/config_manager.py
+++ src/utils/config_manager.py
@@ -5,10 +5,21 @@ class ConfigManager:
     def __init__(self, path="config/config.yaml"):
         self.path = path
         self.config = None
-        self.load_config()
+        self._load_config()
+        self._fix_paths()
 
-    def load_config(self):
+    def _load_config(self):
         base_dir = os.path.dirname(os.path.dirname(__file__))
         absolute_path = os.path.join(base_dir, self.path)
         with open(absolute_path, 'r') as file:
             self.config = yaml.safe_load(file)
+
+    def _extract_path(self):
+        original_path = self.config.get('path', [None])[0]
+        if not original_path:
+            return
+
+        if "[user]" in original_path:
+            user = getpass.getuser()
+            replaced_path = original_path.replace("[user]", user)
+            self.config["path"][0] = replaced_path
