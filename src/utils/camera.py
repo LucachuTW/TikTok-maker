21f18a74abff6cd6cdde2238b6358aeef9d386e9
sergioooo0 <sergiogonzalezrodriguez05@gmail.com>
2025-04-05 12:16:44 +0200

    download camara files

--- src/utils/camera.py
+++ src/utils/camera.py
@@ -3,6 +3,7 @@ import pyudev
 import os
 import subprocess
 from utils.config_manager import ConfigManager
+import shutil
 
 config = ConfigManager()
 
@@ -56,3 +57,62 @@ class Camera:
             print(f"Camera mounted at {mount_path}")
         except subprocess.CalledProcessError:
             print(f"Failed to mount {partition} at {mount_path}")
+
+    def unmount(self):
+        if hasattr(self, 'mount_point'):
+            try:
+                subprocess.run(
+                    ["umount", self.mount_point],
+                    check=True
+                )
+                print(f"Camera unmounted from {self.mount_point}")
+                # Clean up the mount point
+                os.rmdir(self.mount_point)
+            except subprocess.CalledProcessError:
+                print(f"Failed to unmount {self.mount_point}")
+        else:
+            print("No mount point to unmount.")
+
+    def download(self, base_path):
+        """
+        Download the content of the camera to the given path.
+        If there are already files in the path, we can use rsync to only download the new files.
+        """
+        if not hasattr(self, 'mount_point'):
+            print("Camera is not mounted.")
+            return
+
+        camara_path = os.path.join(self.mount_point, "DCIM")
+        if not os.path.exists(camara_path):
+            print(f"Camera path {camara_path} does not exist.")
+            return
+        video_exts = ('.mp4', '.mov', '.avi', '.mkv', '.mts')
+        gcsv_exts = ('.gcsv',)
+
+        video_dest = os.path.join(base_path, "videos")
+        gcsv_dest = os.path.join(base_path, "gcsv")
+
+        os.makedirs(video_dest, exist_ok=True)
+        os.makedirs(gcsv_dest, exist_ok=True)
+
+        print(f"Copying files from {camara_path} to:")
+        print(f"  - Videos: {video_dest}")
+        print(f"  - GCSV : {gcsv_dest}")
+
+        for root, _, files in os.walk(camara_path):
+            for file in files:
+                src_file = os.path.join(root, file)
+                lower_file = file.lower()
+
+                if lower_file.endswith(video_exts):
+                    dst_file = os.path.join(video_dest, file)
+                elif lower_file.endswith(gcsv_exts):
+                    dst_file = os.path.join(gcsv_dest, file)
+                else:
+                    continue  # ignorar otros archivos
+
+                if not os.path.exists(dst_file):
+                    shutil.copy2(src_file, dst_file)
+                    print(f"  Copy: {file}")
+                else:
+                    print(f"  Already exists: {file}")
