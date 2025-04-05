20c76e4f517eb0162984526dc6b1f744bd0acad4
MiniX16 <javivi004.ou@gmail.com>
2025-04-05 11:00:19 +0200

    camera class

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 """Executable module for connecting"""
 from utils.config_manager import ConfigManager
+from utils.camera import Camera
 
 config = ConfigManager()
+camera = Camera()
+print(camera.model)
 print(config.config)
 
 if __name__ == "__main__":
--- src/utils/camera.py
+++ src/utils/camera.py
@@ -9,21 +9,23 @@ class Camera:
         self.vendor = None
         self.model = None
         self.device_node = None
-        self._wait_for_camera(target_model)
+        self._wait_for_camera()
 
-    def _wait_for_camera(self, target_model):
+    def _wait_for_camera(self):
         context = pyudev.Context()
         monitor = pyudev.Monitor.from_netlink(context)
         monitor.filter_by('block')
 
-        print(f"Waiting for USB camera with model '{target_model}'...")
+        known_models = config.config.get("cameras", [])
+
+        print(f"Waiting for USB camera. Known models: {known_models}")
 
         for device in iter(monitor.poll, None):
             if device.action == 'add' and device.get('ID_USB_DRIVER') == 'usb-storage':
                 model = device.get('ID_MODEL', '')
-                if model in config.config.get('camaras', None):
+                if model.lower() in [m.lower() for m in known_models]:
                     self.vendor = device.get('ID_VENDOR', 'Unknown')
                     self.model = model
                     self.device_node = device.device_node
-                    print("Camera detected.")
+                    print(f"Camera detected: {self.vendor} {self.model}")
                     break
