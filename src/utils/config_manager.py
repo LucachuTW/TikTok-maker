ab9e37ce186edd0328a2e2350c5e9de3451160c4
MiniX16 <javivi004.ou@gmail.com>
2025-04-05 12:25:49 +0200

    auto create config

--- src/utils/config_manager.py
+++ src/utils/config_manager.py
@@ -6,8 +6,24 @@ class ConfigManager:
     def __init__(self, path="config/config.yaml"):
         self.path = path
         self.config = None
+        self._ensure_config_exists()
         self._load_config()
         self._fix_paths()
+
+    def _ensure_config_exists(self):
+        base_dir = os.path.dirname(os.path.dirname(__file__))
+        absolute_path = os.path.join(base_dir, self.path)
+
+        config_dir = os.path.dirname(absolute_path)
+        os.makedirs(config_dir, exist_ok=True)
+
+        if not os.path.isfile(absolute_path):
+            default_config = {
+                "path": ["/home/[user]/camera_mount"],
+                "cameras": ["Wasintek_camera"]
+            }
+            with open(absolute_path, 'w') as file:
+                yaml.dump(default_config, file)
 
     def _load_config(self):
         base_dir = os.path.dirname(os.path.dirname(__file__))
