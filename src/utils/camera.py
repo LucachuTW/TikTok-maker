c7d88629b37d231b7403b05658de4b21c87313f0
MiniX16 <javivi004.ou@gmail.com>
2025-04-05 09:42:05 +0200

    detect cam

--- src/utils/camera.py
+++ src/utils/camera.py
@@ -1,1 +1,9 @@
 """Utils focused on detecting and connecting to the camera"""
+import pyudev
+
+def detect_cam():
+    context = pyudev.Context()
+    for device in context.list_devices(subsystem='usb'):
+        if 'GoPro' in str(device):
+            print("GoPro detectada:", device)
+
