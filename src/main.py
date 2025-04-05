48db9506cdde9807328a4647e85630a268f0bed2
MiniX16 <javivi004.ou@gmail.com>
2025-04-05 10:47:49 +0200

    camera detected correctly

--- src/main.py
+++ src/main.py
@@ -1,9 +1,7 @@
 """Executable module for connecting"""
-from utils.camera import detect_cam
 from utils.config_manager import ConfigManager
 
 config = ConfigManager()
-
 print(config.config)
 
 if __name__ == "__main__":
--- src/utils/camera.py
+++ src/utils/camera.py
@@ -1,12 +1,29 @@
 """Utils focused on detecting and connecting to the camera"""
-import os
 import pyudev
-import yaml
+from utils.config_manager import ConfigManager
 
+config = ConfigManager()
 
-def detect_cam():
-    context = pyudev.Context()
-    for device in context.list_devices(subsystem='usb'):
-        if 'GoPro' in str(device):
-            print("GoPro detectada:", device)
+class Camera:
+    def __init__(self):
+        self.vendor = None
+        self.model = None
+        self.device_node = None
+        self._wait_for_camera(target_model)
 
+    def _wait_for_camera(self, target_model):
+        context = pyudev.Context()
+        monitor = pyudev.Monitor.from_netlink(context)
+        monitor.filter_by('block')
+
+        print(f"Waiting for USB camera with model '{target_model}'...")
+
+        for device in iter(monitor.poll, None):
+            if device.action == 'add' and device.get('ID_USB_DRIVER') == 'usb-storage':
+                model = device.get('ID_MODEL', '')
+                if model in config.config.get('camaras', None):
+                    self.vendor = device.get('ID_VENDOR', 'Unknown')
+                    self.model = model
+                    self.device_node = device.device_node
+                    print("Camera detected.")
+                    break
